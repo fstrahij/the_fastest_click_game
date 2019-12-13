5eccd0616cc64b1b6ecd4c87651ae6cd67830f66
fstrahij <filip2893@gmail.com>
2019-12-13 17:48:57 +0100

    Scoreboard added

---
+++ client/src/components/Scoreboard/index.vue
@@ -0,0 +1,82 @@
+<template>
+	<div class="container">
+		<div>
+			<p>YOUR SCORE</p>
+			<p>{{ score }}</p>
+		</div>
+		<p>PLAYER | SCORE</p>
+		<ol>
+			<li 
+				v-for="player in players"
+				:key="player.id"
+			>
+				{{ player.name }} - {{ player.score }}
+			</li>
+		</ol>
+		<button @click="PlayAgain()">Play Again</button>
+	</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Scoreboard',
+    data () {
+        return {
+        	score: 0,
+            players: []
+        }
+    },
+    methods: {
+    	PlayAgain(){    		
+                const remain = 3;
+                this.$store.commit('setRemain', remain);
+    	},
+    	async PostData() {
+    		const baseURI = "http://localhost:3000/newPlayer";
+    		const name = this.$store.getters.getPlayer;;
+    		const score = this.$store.getters.getScore;
+    		this.score = score;
+	    	await axios.post(baseURI, {
+	    		name: name,
+	    		score: score
+	    	}).then(response => {
+	    		this.GetData();
+	    	})
+    	},
+    	GetData() {
+    		const baseURI = "http://localhost:3000/scoreboard";
+	    	axios.get(baseURI).then(response => {
+				this.players = response.data;
+    		})
+    	}
+    },
+    mounted(){
+    	this.PostData();
+    }
+}
+
+</script>
+
+<style scoped>
+.container {
+	background-color: rgb(0,0,0);
+	background-color: rgba(0,0,0, 0.4);
+	color: white;
+	font-weight: bold;
+	border: 3px solid #f1f1f1;
+	position: absolute;
+	top: 40%;
+	left: 50%;
+	height: 50%;
+	transform: translate(-50%, -50%);
+	z-index: 2;
+	width: 80%;
+	padding: 20px;
+	text-align: center;
+}
+h1, h2 {
+  font-weight: normal;
+}
+</style>
